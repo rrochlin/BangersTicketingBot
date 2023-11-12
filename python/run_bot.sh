108c9faf05632cfa76e89b343ed63d22a90f1a14
Robert <rrochlin@yahoo.com>
2023-11-12 06:52:11 +0000

    fixing bash script

--- python/run_bot.sh
+++ python/run_bot.sh
@@ -1,1 +1,5 @@
-../env/bin/python3 main.py &
+#!/bin/sh
+cd /home/raspi/Documents/BangersTicketingBot
+source /env/bin/activate
+cd /home/raspi/Documents/BangersTicketingBot/python
+python3 main.py &
